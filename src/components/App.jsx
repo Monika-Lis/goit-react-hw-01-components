8fce5356a940dc288197b6591d91ebbafff30831
Monika-Lis <lis.monikak@gmail.com>
2024-04-02 19:40:02 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
-import { Profile } from './Profile.jsx';
+import { Profile } from './Profile';
 import user from './user.json';
 
 export const App = () => {
-  return;
-  <>
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
-  </>;
+  return (
+    <div className="background">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,29 @@
-export const Profile = () => {
-  <div class="profile">
-    <div class="description">
-      <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-        alt="User avatar"
-        class="avatar"
-      />
-      <p class="name">Petra Marica</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
-    </div>
+import user from './user.json';
 
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">1000</span>
-      </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">2000</span>
-      </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">3000</span>
-      </li>
-    </ul>
-  </div>;
+export const Profile = props => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={user.avatar} alt="User avatar" className="avatar" />
+        <p className="name">{user.username}</p>
+        <p className="tag">@{user.tag}</p>
+        <p className="location">{user.location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{user.stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{user.stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
